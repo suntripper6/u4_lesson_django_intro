9957912633776015e96651b3a952251e9b1a1af8
Jason Bundy <jason.bundy@me.com>
2023-03-29 17:44:16 -0400

    adding in the artist form

--- tunr_project/tunr/urls.py
+++ tunr_project/tunr/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.artist_list, name='artist_list'),
     path('songs/', views.song_list, name='song_list'),
     path('artists/<int:pk>', views.artist_detail, name='artist_detail'),
-    path('songs/<int:pk>', views.song_detail, name='song_detail')
+    path('songs/<int:pk>', views.song_detail, name='song_detail'),
+    path('artists/new', views.artist_create, name='artist_create'),
 ]
--- tunr_project/tunr/views.py
+++ tunr_project/tunr/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Artist, Song
+from .forms import ArtistForm
 
 # Create your views here.
 
@@ -22,3 +23,14 @@ def song_list(request):
 def song_detail(request, pk):
     song = Song.objects.get(id=pk)
     return render(request, 'tunr/song_detail.html', {'song': song})
+
+
+def artist_create(request):
+    if request.method == 'POST':
+        form = ArtistForm(request.POST)
+        if form.is_valid():
+            artist = form.save()
+            return redirect('artist_detail', pk=artist.pk)
+    else:
+        form = ArtistForm()
+    return render(request, 'tunr/artist_form.html', {'form': form})
